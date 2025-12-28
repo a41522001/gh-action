c743180c1017bc44542ad9875900020d0a8d15fd
a41522001 <a41522001@gmail.com>
2025-12-28 14:52:29 +0800

    github action init

---
+++ src/__tests__/App.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect, test } from 'vitest';
+import { sum } from '@/utils';
+describe('add', () => {
+  test('add1', () => {
+    expect(sum(3, 5)).toBe(8);
+  });
+  test('add2', () => {
+    expect(sum(-5, 5)).toBe(0);
+  });
+});
